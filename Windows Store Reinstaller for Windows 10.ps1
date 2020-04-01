ac7e4f35dd7a2201ad6a8ae3f31d08ad4d44b408
Alex Cornejo <AlexRabbit@users.noreply.github.com>
2020-03-31 20:32:00 -0600

    Moved to another directory.

--- Windows Store Reinstaller for Windows 10.ps1
+++
@@ -1,5 +0,0 @@
-﻿Start-Process WSReset.exe
-
-Get-AppXPackage *WindowsStore* -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
-
-Start-Process WSReset.exe
